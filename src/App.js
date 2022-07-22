89c1b26545465d30ce12fbd7f3531c56d3d81d32
Reynolds Sharp <sharpdesignco@Reynoldss-iPro.attlocal.net>
2022-07-21 19:46:05 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Alert from 'react-bootstrap/Alert';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Features from './Components/Features.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  // const [posts, setPosts] = useState([]);
+  // const [title, setTitle] = useState('');
+  // const [body, setBody] = useState('');
+
+  const [houses, setHouses] = useState([]);
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+
+  // GET with fetch API // GET HOUSES
+  useEffect(() => {
+    const fetchHouse = async () => {
+      const response = await fetch(
+          'https://ancient-taiga-31359.herokuapp.com/api/houses'
+      );
+      const data = await response.json();
+      console.log(data);
+      setHouses(data);
+    };
+    fetchHouse();
+  }, []);
+
+  
+
+  // Delete with fetchAPI
+  const deleteHouse = async (_id) => {
+    let response = await fetch(
+      `https://ancient-taiga-31359.herokuapp.com/api/houses/${_id}`,
+      {
+          method: 'DELETE',
+      }
+    );
+    if (response.status === 200) {
+      setHouses(
+          houses.filter((house) => {
+            return house._id !== _id;
+          })
+      );
+    } else {
+      return;
+    }
+  };
+
+  // Post with fetchAPI
+  const addHouses = async (name) => {
+    let response = await fetch('https://ancient-taiga-31359.herokuapp.com/api/houses', {
+      method: 'POST',
+      body: JSON.stringify({
+          name: name,
+          room: room,
+          // userId: Math.random().toString(36).slice(2),
+      }),
+      headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    let data = await response.json();
+    setHouses((houses) => [data, ...houses]);
+    setName('');
+    setRoom('');
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addHouses(name);
+  };
+ 
+ return (
+    <Container>
+    <br></br>
+    <h1>The House Place</h1>
+    <div className="app">
+    <div className="add-post-container">
+      <br />
+      <form onSubmit={handleSubmit}>
+          <input type="text" className="form-control" value={name}
+            onChange={(e) => setName(e.target.value)}
+          /><br /><br />
+          <textarea name="" className="form-control" id="" cols="25" rows="10" 
+            value={room} onChange={(e) => setRoom(e.target.value)} 
+          ></textarea>
+          <br /><br />
+          <Button className='btn-success' type="submit">Add House</Button>
+      </form>
     </div>
-  );
-}
+    <div className="posts-container">
+          {houses.map((house) => {
+              return (
+
+                <Card className="post-card" key={house._id}>
+                    <Card.Header>
+                    <br></br>
+                    <h2 className="post-title">{house.name}</h2>
+                    <br></br>
+                    </Card.Header>
+                    <br></br>
+                    <Card.Body className="post-body">{house.room}</Card.Body>
+                    <Button className='btn-dark btn-sm post-btn' type='button'>
+                      <div className="button">
+                        <div className="delete-btn " onClick={() => deleteHouse(house._id)}>
+                          Delete
+                        </div>
+                      </div>    
+                    </Button> 
+                    <br></br> 
+                </Card>
+
+              );
+          })}
+        </div>
+    </div>
+    {/* <Features />  -- removed for now to test houses  */ }
+    </Container>
+    );
+};
 
 export default App;
---
+++ src/Components/Features.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Features = () => {
+    return (
+        <>
+  {/* Feature item */}
+  <div className="features-container">
+    <div className="container__image">
+    <img src="https://cdn.psychologytoday.com/sites/default/files/styles/article-inline-half-caption/public/field_blog_entry_images/2018-09/shutterstock_648907024.jpg?itok=0hb44OrI" alt="Girl in a jacket"/>
+    </div>
+    <div className="container__feature">
+      <h4>Features Name goes here</h4>
+      <p>repudiandae veniam quaerat sunt sed alias aut fugiat sit autem sed est voluptatem omnis possimus esse voluptatibus quis est aut tenetur dolor nequerepudiandae veniam quaerat sunt sed alias aut fugiat sit autem sed est voluptatem omnis possimus esse voluptatibus quis est aut tenetur dolor nequerepudiandae veniam quaerat sunt sed alias aut fugiat sit autem sed est voluptatem omnis possimus esse voluptatibus quis est aut tenetur dolor neque</p>
+    </div>
+  </div>
+  
+
+    {/* Feature item */}
+    <div className="features-container-rev">
+    <div className="container__image left-image">
+    <img src="https://cdn.psychologytoday.com/sites/default/files/styles/article-inline-half-caption/public/field_blog_entry_images/2018-09/shutterstock_648907024.jpg?itok=0hb44OrI" alt="Girl in a jacket"/>
+    </div>
+    <div className="container__feature">
+      <h4>Features Name goes here</h4>
+      <p>repudiandae veniam quaerat sunt sed alias aut fugiat sit autem sed estrepudiandae veniam quaerat sunt sed alias aut fugiat sit autem sed est voluptatem omnis possimus esse voluptatibus quis est aut tenetur dolor nequerepudiandae veniam quaerat sunt sed alias aut fugiat sit autem sed est voluptatem omnis possimus esse voluptatibus quis est aut tenetur dolor neque voluptatem omnis possimus esse voluptatibus quis est aut tenetur dolor neque</p>
+    </div>
+  </div>
+  
+ 
+</>
+
+    )
+}
+
+export default Features;
